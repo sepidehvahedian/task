cc69450cdf19bb7042e53384ea80a64b4873a80a
sepideh <sepideh0771@gmail.com>
2025-03-25 03:45:26 -0700

    first route app

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-
+// routes
+app.get("/hello", (req, res) => {
+  console.log(res);
+  res.send("TASK MANAGER APP");
+});
 
 const port = 3000;
-app.listen(port,console.log(`server is listening on port ${port}...`))
+app.listen(port, console.log(`server is listening on port ${port}...`));
